424a6a30279879ce63250ca015c05aa48195fb02
Camila <maria.parram@udea.edu.co>
2025-09-19 12:07:42 -0500

    Declara clase Actividad y contiene una lista de actividades de ejemplo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+object Main extends App {
+  // Declaracion de la clase Actividad
+  case class Actividad(usuario: String, tipo: String, duracionMin: Int, calorias: Int, dia: String)
 
-def msg = "I was compiled by Scala 3. :)"
+  // Lista de actividades de ejemplo
+  val actividades = List(
+    Actividad("Juan", "Correr", 30, 300, "Lunes"),
+    Actividad("Juan", "Natación", 45, 400, "Miércoles"),
+    Actividad("Ana", "Ciclismo", 60, 500, "Martes")
+  )
+
+  println(actividades)
+}
