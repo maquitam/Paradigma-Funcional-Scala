d796ff650b5706552a6bfb1fa9eaafaaa866b483
Sebas <sebastian.cardona7@udea.edu.co>
2025-09-20 21:54:13 -0500

    Se creo la funcion totalTiempo

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,27 @@
+object Main extends App {
+  // Declaracion de la clase Actividad
+  case class Actividad(usuario: String, tipo: String, duracionMin: Int, calorias: Int, dia: String)
+
+  // Lista de actividades de ejemplo
+  val actividades = List(
+    Actividad("Juan", "Correr", 30, 300, "Lunes"),
+    Actividad("Juan", "Natación", 45, 400, "Miércoles"),
+    Actividad("Ana", "Ciclismo", 60, 500, "Martes")
+  )
+
+  // Obtiene las actividades por usuario y las retorna como una lista
+  def actividadesPorUsuario(usuario: String, actividades: List[Actividad]) : List[Actividad] = {
+    actividades.filter(_.usuario == usuario)
+  }
+
+  // Esta funcion obtiene las calorias del usuario y retorna el total de calorias por usuario
+  def totalCalorias(usuario: String, actividades: List[Actividad]) : Int = {
+    actividadesPorUsuario(usuario, actividades)
+      .map(_.calorias)
+      .sum
+  }
+  //
+  def totalTiempo(usuario: String, actividades: List[Actividad]): Int = {
+    actividadesPorUsuario(usuario, actividades)
+  }
+}
